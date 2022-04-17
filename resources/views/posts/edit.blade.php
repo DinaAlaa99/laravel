5edd81b41d73f0731c8f3c15ca372b0cb23cd91c
dina <dinaalaawaly@gmail.com>
2022-04-17 10:57:51 -0400

    update edit view

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,19 +3,20 @@
 @section('title')Edit @endsection
 
 @section('content')
-<form method="PUT" action="{{ route('posts.update', ['post' => $posts['id']]) }}">
+<form method="Post" action="{{ route('posts.update', ['post' => $posts['id']]) }}">
             @csrf
+            @method('PUT')
             <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Title</label>
+                <label for="exampleFormControlInput1" class="form-label" name="Title">Title</label>
                 <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
             </div>
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+                <label for="exampleFormControlTextarea1" class="form-label" name="Description">Description</label>
                 <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
             </div>
 
             <div class="mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Post Creator</label>
+                <label for="exampleFormControlTextarea1" class="form-label" name="Creator">Post Creator</label>
                 <select class="form-control">
                     <option value="1" {{ $posts['id'] == 1 ? 'selected' : '' }}>Ahmed</option>
                     <option value="2" {{ $posts['id'] == 2 ? 'selected' : '' }}>Mohamed</option>
