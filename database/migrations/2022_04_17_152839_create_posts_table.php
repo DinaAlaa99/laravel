dc36445efeff4b1fa17dae30232fb29047221c43
dina <dinaalaawaly@gmail.com>
2022-04-17 18:44:08 -0400

    remove down method

--- database/migrations/2022_04_17_152839_create_posts_table.php
+++ database/migrations/2022_04_17_152839_create_posts_table.php
@@ -21,13 +21,5 @@ class CreatePostsTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('posts');
-    }
+
 }
