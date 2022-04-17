9b84f75ee63703bb9fd02e1dfa383b914d05761e
dina <dinaalaawaly@gmail.com>
2022-04-17 00:57:44 -0400

    show edit update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
-{
+{   
+    
+    
     public function index()
     {
         $posts = [
@@ -33,6 +35,33 @@ class PostController extends Controller
 
     public function show($postId)
     {
-        return $postId;
+        $posts = [
+            ['id' => 1, 'title' => 'Laravel', 'post_creator' => 'Ahmed', 'created_at' => '2022-04-16 10:37:00'],
+            ['id' => 2, 'title' => 'PHP', 'post_creator' => 'Mohamed', 'created_at' => '2022-04-16 10:37:00'],
+            ['id' => 3, 'title' => 'Javascript', 'post_creator' => 'Ali', 'created_at' => '2022-04-16 10:37:00'],
+        ];
+        
+        // dd($posts); for debugging
+        return view('posts.show',[
+            'posts' => $posts[$postId-1],
+        ]);
+       
+    }
+    public function edit($postId)
+    {
+        $posts = [
+            ['id' => 1, 'title' => 'Laravel', 'post_creator' => 'Ahmed', 'created_at' => '2022-04-16 10:37:00'],
+            ['id' => 2, 'title' => 'PHP', 'post_creator' => 'Mohamed', 'created_at' => '2022-04-16 10:37:00'],
+            ['id' => 3, 'title' => 'Javascript', 'post_creator' => 'Ali', 'created_at' => '2022-04-16 10:37:00'],
+        ];
+        
+        // dd($posts); for debugging
+        return view('posts.edit',[
+            'posts' => $posts[$postId-1],
+        ]);
+       
+    }
+    public function update(){
+        return view('posts.update');
     }
 }
